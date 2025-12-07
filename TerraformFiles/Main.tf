f6b23166dd2c786c8f7a2ad9b52fd2750541e3cd
Ashwini018 <152056171+Ashwini018@users.noreply.github.com>
2025-12-07 14:02:12 +0000

    *

--- TerraformFiles/Main.tf
+++ TerraformFiles/Main.tf
@@ -28,6 +28,14 @@ resource "azurerm_subnet" "subnet" {
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["10.0.1.0/24"]
 }
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.vm_name}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "nic" {
     name = "${var.vm_name}-nic"
     resource_group_name = azurerm_resource_group.rg.name
@@ -36,6 +44,7 @@ resource "azurerm_network_interface" "nic" {
         name = "internal"
         subnet_id = azurerm_subnet.subnet.id
         private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.public_ip.id
     }
 }
 resource "azurerm_linux_virtual_machine" "vm" {
